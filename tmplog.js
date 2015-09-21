0e409819cb3eed70357b78ea5cc9069fa9777b4a
Erik Wong <ewong@blizzard.com>
2015-09-21 10:23:02 -0700

    change were parsing happens

--- tmplog.js
+++ tmplog.js
@@ -6,10 +6,12 @@ var devices = fs.readdirSync(basePath);
 var device = devices[0];
 
 fs.readFile(path.join(basePath, device, 'w1_slave'), function(err, readOut) {
-  var temp = cToF(String(readOut).match(/t=\d*/)[0].replace('t=', ''));
+  var rawTempString = String(readOut).match(/t=\d*/)[0].replace('t=', '');
+  console.log(rawTempString);
+  var temp = cToF(rparseFloat( awTempString ) / 1000);
   console.log(temp);
 });
 
 var cToF = function(temp) {
-  return parseFloat(temp) / 100 * 9.0 / 5.0 + 32.0;
+  return temp * 9.0 / 5.0 + 32.0;
 }
