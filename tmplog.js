e39364cd715130d7a04e88b41396775d7a80e58d
Erik Wong <ewong@blizzard.com>
2015-09-21 10:17:52 -0700

    add conversion

--- tmplog.js
+++ tmplog.js
@@ -5,6 +5,11 @@ var basePath = '/sys/bus/w1/devices';
 var devices = fs.readdirSync(basePath);
 var device = devices[0];
 
-fs.readFile(path.join(basePath, device, 'w1_slave'), function(err, temp) {
+fs.readFile(path.join(basePath, device, 'w1_slave'), function(err, readOut) {
+  var temp = cToF(readOut.match(/t=\d*/)[0].replace('t=', ''));
   console.log(temp);
 });
+
+var cToF = function(temp) {
+  return parseFloat(temp) * 9.0 / 5.0 + 32.0;
+}
